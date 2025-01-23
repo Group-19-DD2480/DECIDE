b553c33201040d2a4f610e91acba21c845cdd81b
Ruben Socha <ruben@socha.se>
2025-01-23 20:57:45 +0100

    feat: implemented lic_5 and tests, closing #9

--- src/lic.py
+++ src/lic.py
@@ -1,1 +1,18 @@
 
+
+def lic_5(points: list[tuple[float, float]]) -> bool:
+    '''
+    There exists at least one set of two consecutive data points,
+    (X[i],Y[i]) and (X[j],Y[j]), such
+    that X[j] - X[i] < 0. (where i = j-1)
+
+    Parameters:
+        points (list[tuple[float, float]]): List of 2D points [(x_1, y_1), (x_2, y_2), ...].
+
+    Returns:
+        bool: True if the x values of two consecutive data points are decreasing, False otherwise.
+    '''
+    for (x_i, _), (x_j, _) in zip(points, points[1:]):
+        if x_j - x_i < 0:
+            return True
+    return False
--- tests/test_lic.py
+++ tests/test_lic.py
@@ -1,1 +1,18 @@
 from src.lic import *
+
+
+def test_lic_5() -> None:
+    '''
+    There exists at least one set of two consecutive data points,
+    (X[i],Y[i]) and (X[j],Y[j]), such
+    that X[j] - X[i] < 0. (where i = j-1)
+    '''
+    points = [(1, 0), (0, 0)]
+    assert lic_5(points)
+    points = [(2, 3), (3, 4), (2, 4)]
+    assert lic_5(points)
+
+    points = [(0, 0), (1, 0)]
+    assert not lic_5(points)
+    points = [(1, 2), (3, 4), (4, 5)]
+    assert not lic_5(points)
