9491a668c46344befff01b724f4abad702bd6eee
HermanKassler <tore.gaani@gmail.com>
2025-01-24 11:29:50 +0100

    test: feat: implement LIC-3 with unit tests. Closes #5
    add documentation with description for parameters and return value

--- src/lic.py
+++ src/lic.py
@@ -1,1 +1,54 @@
+import numpy as np
 
+
+def lic_3(points: list[tuple[float, float]], area: float) -> bool:
+    '''
+    There exists at least one set of three consecutive data points that
+    are the vertices of a triangle with area greater than AREA1.
+    (0 <= AREA1)
+
+    Parameters:
+        points (list[tuple[float, float]]): List of 2D points [(x_1, y_1), (x_2, y_2), ...].
+        area: Area that the triangle should be larger than
+
+    Returns:
+        bool: True if the area of a triangle of any three consecutive
+              data points is strictly greater than the given area,
+              false otherwise
+    '''
+    if area < 0:
+        return False
+
+    for set in zip(points, points[1:], points[2:]):
+        set = np.array(set)
+        p1 = set[0]
+        p2 = set[1]
+        p3 = set[2]
+        area_of_triangle = (1/2) * abs(
+                                       p1[0] * (p2[1] - p3[1]) +
+                                       p2[0] * (p3[1] - p1[1]) +
+                                       p3[0] * (p1[1] - p2[1])
+                                       )
+        if area_of_triangle > area:
+            return True
+
+    # No sets create a large enough triangle
+    return False
+
+def lic_5(points: list[tuple[float, float]]) -> bool:
+    '''
+    There exists at least one set of two consecutive data points,
+    (X[i],Y[i]) and (X[j],Y[j]), such
+    that X[j] - X[i] < 0. (where i = j-1)
+
+    Parameters:
+        points (list[tuple[float, float]]): List of 2D points [(x_1, y_1), (x_2, y_2), ...].
+
+    Returns:
+        bool: True if the x values of two consecutive data points are decreasing, False otherwise.
+    '''
+    for (x_i, _), (x_j, _) in zip(points, points[1:]):
+        if x_j - x_i < 0:
+            return True
+
+    return False
--- tests/test_lic.py
+++ tests/test_lic.py
@@ -1,1 +1,41 @@
 from src.lic import *
+
+
+def test_lic_3() -> None:
+    '''
+    There exists at least one set of three consecutive data points that
+    are the vertices of a triangle with area greater than AREA1.
+    (0 <= AREA1)
+    '''
+
+    # Invalid area
+    area = -1.0
+    points = [(0, 0), (0, 1), (1, 2)]
+    assert not lic_3(points, area)
+
+    # Traingle is larger than area
+    area = 1.0
+    points = [(0, 0), (3, 0), (0, 3)]
+    assert lic_3(points, area)
+
+    # Exact area
+    area = 1.0
+    points = [(0, 0), (2, 0), (0, 1)]
+    assert not lic_3(points, area)
+
+
+def test_lic_5() -> None:
+    '''
+    There exists at least one set of two consecutive data points,
+    (X[i],Y[i]) and (X[j],Y[j]), such
+    that X[j] - X[i] < 0. (where i = j-1)
+    '''
+    points = [(1, 0), (0, 0)]
+    assert lic_5(points)
+    points = [(2, 3), (3, 4), (2, 4)]
+    assert lic_5(points)
+
+    points = [(0, 0), (1, 0)]
+    assert not lic_5(points)
+    points = [(1, 2), (3, 4), (4, 5)]
+    assert not lic_5(points)
