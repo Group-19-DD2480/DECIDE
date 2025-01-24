46a9292c0dd07afacd30cba94904ea4356a80422
erikgsmit <erikgsmit@gmail.com>
2025-01-24 12:09:15 +0100

    test: added test cases for lic_0

--- tests/test_lic.py
+++ tests/test_lic.py
@@ -1,1 +1,29 @@
-from src.lic import *
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from lic import *
+
+
+def test_lic_0() -> None:
+    """
+    There exists at least one set of two consecutive data points that are a distance greater than
+    the length, LENGTH1, apart.
+    (0 ≤ LENGTH1)
+    """
+
+    # Valid test case
+    points = [(0, 0), (0, 1), (0, 2), (0, 3)]
+    length_1 = 2
+    assert lic_0(points, length_1) == True
+
+    # Invalid test case
+    points = [(0, 0), (0, 5), (10, 10), (100, 100)]
+    length_1 = 2
+    assert lic_0(points, length_1) == False
+
+    # Single point
+    points = [(0, 0)]
+    length_1 = 2
+    assert lic_0(points, length_1) == False
