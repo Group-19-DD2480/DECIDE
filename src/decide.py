f0edf15173b250aa8753571933e28a22f5fc340d
Erik Smit <132951056+erikgsmit@users.noreply.github.com>
2025-01-28 12:15:25 +0100

    update: moved PARAMETERS_T to seperate file and added import statements, closes #58 (#59)

--- src/decide.py
+++ src/decide.py
@@ -1,7 +1,3 @@
 from collections import namedtuple
 
-PARAMETERS_T = namedtuple('PARAMETERS_T', [
-    'length_1', 'radius_1', 'epsilon', 'area_1', 'q_pts', 'quads', 
-    'dist', 'n_pts', 'k_pts', 'a_pts', 'b_pts', 'c_pts', 
-    'd_pts', 'e_pts', 'f_pts', 'g_pts', 'length_2', 'radius_2', 'area_2'
-])
+from parameters import PARAMETERS_T
---
+++ src/parameters.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+@dataclass
+class PARAMETERS_T:
+    length_1: float = 1.0
+    radius_1: float = 1.0
+    epsilon: float = 0.5
+    area_1: float = 1.0
+    q_pts: int = 2
+    quads: int = 1
+    dist: float = 1.0
+    n_pts: int = 3
+    k_pts: int = 1
+    a_pts: int = 1
+    b_pts: int = 1
+    c_pts: int = 1
+    d_pts: int = 1
+    e_pts: int = 1
+    f_pts: int = 1
+    g_pts: int = 1
+    length_2: float = 1.0
+    radius_2: float = 1.0
+    area_2: float = 1.0
