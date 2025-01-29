47ff92bd89584123975e914760bf77fd88f0fdc0
Ruben Socha <ruben@socha.se>
2025-01-29 16:32:36 +0100

    refactor: LIC-5 tests

--- tests/test_lic.py
+++ tests/test_lic.py
@@ -181,17 +181,18 @@ def test_lic_4_invalid() -> None:
     assert not lic_4(points, q_pts, quads)
 
 
-def test_lic_5() -> None:
-    """
-    There exists at least one set of two consecutive data points,
-    (X[i],Y[i]) and (X[j],Y[j]), such
-    that X[j] - X[i] < 0. (where i = j-1)
-    """
+"""
+There exists at least one set of two consecutive data points,
+(X[i],Y[i]) and (X[j],Y[j]), such
+that X[j] - X[i] < 0. (where i = j-1)
+"""
+def test_lic_5_positive() -> None:
     points = [(1, 0), (0, 0)]
     assert lic_5(points)
     points = [(2, 3), (3, 4), (2, 4)]
     assert lic_5(points)
 
+def test_lic_5_negative() -> None:
     points = [(0, 0), (1, 0)]
     assert not lic_5(points)
     points = [(1, 2), (3, 4), (4, 5)]
