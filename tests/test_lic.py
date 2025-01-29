3edfba049c89170b99d04c95bfce27c88822d6ed
Ruben Socha <ruben@socha.se>
2025-01-29 16:19:36 +0100

    refactor: LIC-0 tests

--- tests/test_lic.py
+++ tests/test_lic.py
@@ -8,23 +8,25 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"
 from lic import *
 
 
-def test_lic_0() -> None:
-    """
-    There exists at least one set of two consecutive data points that are a distance greater than
-    the length, LENGTH1, apart.
-    (0 ≤ LENGTH1)
-    """
 
-    # Invalid test case: Condition is false (distance ≤ LENGTH1)
-    points = [(0, 0), (0, 1)]
-    length_1 = 2
-    assert not lic_0(points, length_1)
-
+"""
+There exists at least one set of two consecutive data points that are a distance greater than
+the length, LENGTH1, apart.
+(0 ≤ LENGTH1)
+"""
+def test_lic_0_positive() -> None:
     # Valid test case: Condition is true (distance > LENGTH1)
     points = [(0, 0), (0, 5), (10, 10), (100, 100)]
     length_1 = 2
     assert lic_0(points, length_1)
 
+def test_lic_0_negative() -> None:
+    # Invalid test case: Condition is false (distance ≤ LENGTH1)
+    points = [(0, 0), (0, 1)]
+    length_1 = 2
+    assert not lic_0(points, length_1)
+
+def test_lic_0_invalid() -> None:
     # Single point: (raises exception)
     points = [(0, 0)]
     length_1 = 2
